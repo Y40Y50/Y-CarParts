a275b80cc4c2b5edc12779dca60c5f689f782eec
Ahmed Yahya <y50y50@gmail.com>
2026-06-23 20:50:56 +0100

    Add cart quantity controls and product images

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from products.models import Product
 
+
 def add_to_cart(request, product_id):
     cart = request.session.get('cart', {})
 
@@ -50,3 +51,43 @@ def cart_count(request):
     return {
         'cart_count': count
     }
+
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', {})
+
+    product_id = str(product_id)
+
+    if product_id in cart:
+        del cart[product_id]
+
+    request.session['cart'] = cart
+
+    return redirect('cart')
+
+def increase_quantity(request, product_id):
+    cart = request.session.get('cart', {})
+
+    product_id = str(product_id)
+
+    if product_id in cart:
+        cart[product_id] += 1
+
+    request.session['cart'] = cart
+
+    return redirect('cart')
+
+
+def decrease_quantity(request, product_id):
+    cart = request.session.get('cart', {})
+
+    product_id = str(product_id)
+
+    if product_id in cart:
+        cart[product_id] -= 1
+
+        if cart[product_id] <= 0:
+            del cart[product_id]
+
+    request.session['cart'] = cart
+
+    return redirect('cart')
--- y_carparts/urls.py
+++ y_carparts/urls.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from django.urls import path
 from cart.views import add_to_cart
-from cart.views import cart_view
+from cart.views import cart_view, remove_from_cart
+from django.conf import settings
+from django.conf.urls.static import static
+
 from products.views import (
     home,
     product_list,
     product_detail,
+)
+
+from cart.views import (
+    cart_view,
+    add_to_cart,
+    remove_from_cart,
+    increase_quantity,
+    decrease_quantity,
 )
 
 urlpatterns = [
@@ -33,5 +44,26 @@ urlpatterns = [
         cart_view,
         name='cart'
     ),
+    path(
+        'cart/remove/<int:product_id>/',
+        remove_from_cart,
+        name='remove_from_cart'
+    ),
+    path(
+        'cart/increase/<int:product_id>/',
+        increase_quantity,
+        name='increase_quantity'
+    ),
+
+    path(
+        'cart/decrease/<int:product_id>/',
+        decrease_quantity,
+        name='decrease_quantity'
+    ),
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
 
