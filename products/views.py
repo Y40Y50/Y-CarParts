ccfb25dd7a256febcd44e3793654794f877c9de2
Ahmed Yahya <y50y50@gmail.com>
2026-06-23 21:10:44 +0100

    Add product detail images and improve product layout

--- products/views.py
+++ products/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from .models import Product, Category
 
 def home(request):
     return render(request, 'home.html')
 
 def product_list(request):
     products = Product.objects.all()
+    categories = Category.objects.all()
 
     return render(
         request,
         'products/product_list.html',
-        {'products': products}
+        {
+            'products': products,
+            'categories': categories,
+        }
     )
 
 def product_detail(request, product_id):
