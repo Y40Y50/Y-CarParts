df57a7617dca6b42e2a7c20798eb772a1969cafd
Ahmed Yahya <y50y50@gmail.com>
2026-06-23 21:23:52 +0100

    Add category filtering and product images

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
 from .models import Product, Category
 
 def home(request):
     return render(request, 'home.html')
 
 def product_list(request):
+    category_id = request.GET.get('category')
+
     products = Product.objects.all()
+
+    if category_id:
+        products = products.filter(category_id=category_id)
+
     categories = Category.objects.all()
 
     return render(
